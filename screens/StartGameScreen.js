cc8f32231cd4ce9aaa67ee82642c88f2271de534
kevin alnizar <96331929+kevinxcode@users.noreply.github.com>
2022-10-22 14:08:56 +0700

    backgound with image

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -7,8 +7,14 @@ function StartGameScreen(){
     return (
     <View style={styles.inputContainer}>
     <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false} />
-     <PrimaryButton>Reset</PrimaryButton>
-     <PrimaryButton>Confirm</PrimaryButton>
+    <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+            <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer} >
+            <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+     </View>
     </View>
     );
 }
@@ -17,6 +23,8 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer:{
+        justifyContent: 'center',
+        alignItems: 'center',
         // flex: 1,
         marginTop: 100,
         marginHorizontal: 24,
@@ -39,5 +47,11 @@ const styles = StyleSheet.create({
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center'
+    },
+    buttonsContainer:{
+        flexDirection: 'row'
+    },
+    buttonContainer:{
+        flex: 1
     }
 });
