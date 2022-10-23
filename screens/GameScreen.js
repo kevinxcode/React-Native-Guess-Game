3070046bf40439e0a17f28efd9b74639864d6fe8
kevin alnizar <96331929+kevinxcode@users.noreply.github.com>
2022-10-23 14:54:07 +0700

    random number

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,18 @@
 import { View, Text, StyleSheet } from 'react-native';
 import Title from '../components/title';
 
+
+function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  
+    if (rndNum === exclude) {
+      return generateRandomBetween(min, max, exclude);
+    } else {
+      return rndNum;
+    }
+  }
+
+
 function GameScreen() {
   return (
     <View style={styles.screen}>
